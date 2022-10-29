b574fc549828db22d584030dddb78e08ef1f86c0
Bartłomiej Zając <bartoss97@gmail.com>
2022-10-29 14:28:06 +0300

    UI setup

--- macrix-frontend/src/app/app.module.ts
+++ macrix-frontend/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PeopleTableComponent } from './people-table/people-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
